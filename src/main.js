fe420cd5e7d64af336cb40b9e67dc5d19135188c
iashchuk <221box@list.ru>
2019-11-18 00:54:35 +0300

    Удаляет лишнее

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 
-import store from "./store/store";
 import routes from "./routes";
 import "./assets/scss/main.scss";
-
 
 Vue.use(VueRouter);
 export const router = new VueRouter({ mode: "history", routes });
@@ -14,6 +12,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount("#app");
